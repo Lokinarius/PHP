b0a178ea6c5c7d1349677aa5cd212a45272db575
William Galvão <Liam_draven@outlook.com>
2025-01-27 17:05:03 -0300

    Create index.php

---
+++ Aula000 - Olá,Mundo!/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello, World! \u{1F30E}";
+            // pode tambem se utilizar o comando "print"
+        ?>
+    </h1>
+</body>
+</html>
